0d6393a430b87e1a916e3b5b6e2e99862f1283c7
Utsav <usingla78@gmail.com>
2019-08-18 11:05:40 +0530

    abhi bhi Raddi hi hai

---
+++ labAP4/UserX.java
@@ -0,0 +1,15 @@
+package labAP4;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+interface UserX{
+//    public void make()throws IOException;
+//    public static int countd=0;
+//    public String getName();
+    public void Search()throws IOException;
+    public void seeReward()throws IOException;
+    public void _helper(Object... values)throws IOException;
+    public void printX();
+
+}
